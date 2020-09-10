8cdf4ffa6914d723c85540adbb643fc0ddb52948
Sam <samtpomarico@gmail.com>
2020-09-09 21:34:47 -0500

    Completed optional final touches

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -10,6 +10,7 @@ export class FavPhotosComponent implements OnInit {
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
   image2 = 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001044/Basset-Hound-On-White-03.jpg';
   image3 = 'https://m.media-amazon.com/images/M/MV5BMTQ5NTI2NTI4NF5BMl5BanBnXkFtZTcwNjk2NDA2OQ@@._V1_UX182_CR0,0,182,268_AL_.jpg';
+  image4 = 'https://i.pinimg.com/originals/e4/26/70/e426702edf874b181aced1e2fa5c6cde.gif'
 
   constructor() { }
 
